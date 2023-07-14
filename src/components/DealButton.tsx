330b1a03489789d8bad3dcea0cf95eb5ab89060f
daro-asecas <daro.1919@gmail.com>
2023-07-14 01:13:41 -0300

    MVP-eslinted

--- src/components/DealButton.tsx
+++ src/components/DealButton.tsx
@@ -1,5 +1,5 @@
 export default function DealButton( {text,callback}:{text:string,callback:Function} ) {
   return (
-    <button type="button" className={`button-${callback.name}`} onClick={() => callback()} >{callback.name}</button>
+    <button type="button" className={`button-${callback.name}`} onClick={() => callback()} >{text}</button>
   );
 }
